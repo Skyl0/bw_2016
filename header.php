a5916327ef42385d159c429c7ac783780e8468bd
sKyl0 <ernst.marc@gmail.com>
2016-10-06 10:57:33 +0200

    added fontawesome

--- header.php
+++ header.php
@@ -13,15 +13,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Bastian Werner Wetterfotografie - <? echo the_title() ?></title>
   <!-- begin header --><?php wp_head(); ?><!-- end header wp -->
-  <!-- Bootstrap -->
+  <!-- Bootstrap / Fontawesome-->
   <link href="<? echo get_template_directory_uri() ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<? echo get_template_directory_uri() ?>/assets/css/font-awesome.min.css" rel="stylesheet">
+  <!-- regular stylesheet -->
   <link href="<? echo get_template_directory_uri() ?>/style.css" rel="stylesheet">
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <!-- Date Time Picker Libraries -->
-  <script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script>
-  <script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-  <link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
 </head>
