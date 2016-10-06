3302268acec947787cb3b356e71e95da32910203
sKyl0 <ernst.marc@gmail.com>
2016-10-06 12:04:45 +0200

    favicons and more

--- footer.php
+++ footer.php
@@ -26,29 +26,23 @@
 </div>
 <div id="footer">
   <div class="wrapper">
-    <div class="col-sm-6 footer-left">&copy; Bastian Werner 2016</div>
+    <div class="col-sm-6 footer-left"><i class="fa fa-copyright" aria-hidden="true"></i> Bastian Werner 2016</div>
     <div class="col-sm-6 footer-right">
+      <a href="./kontakt" class="mail-link">
+        <i class="fa fa-envelope" aria-hidden="true"></i>
+      </a>
       <a href="#" class="scrollup-link">
         <i class="fa fa-chevron-up fa-2x" aria-hidden="true"></i>
       </a>
     </div>
   </div>
 </div>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- other external JS -->
-<script src="https://use.fontawesome.com/55ae683038.js"></script>
 <!-- internal JS -->
 <script src="<? echo get_template_directory_uri() ?>/assets/js/bootstrap.min.js"></script>
 <!-- self written JS -->
 <script src="<? echo get_template_directory_uri() ?>/assets/js/default.js"></script>
 <!-- end JS part -->
 <?php
-/* Always have wp_footer() just before the closing </body>
- * tag of your theme, or you will break many plugins, which
- * generally use this hook to reference JavaScript files.
- */
-
 wp_footer();
 ?>
 </body>
