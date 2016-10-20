9810f4c1f99832b743966e5da528c2c54e1af389
Marc <ernst.marc@gmail.com>
2016-10-20 20:59:48 +0200

    version 1.0.3 of css/js

--- assets/js/default.js
+++ assets/js/default.js
@@ -45,10 +45,10 @@ $(document).ready(function () {
   var floatbox = 'div#float-box-1';
 
 
-  $(floatbox).css({"right": "-1000px", "opacity": "0"});
+  $(floatbox).css({"margin-right": "-1000px", "opacity": "0"});
 
   $(floatbox).removeClass('float-hide');
   $(floatbox).delay(5000).animate({opacity: "1"}, {duration: 1750, queue: false});
-  $(floatbox).delay(5000).animate({right: "0px"}, {duration: 1500, queue: false, easing: "swing"});
+  $(floatbox).delay(5000).animate({"margin-right": "15px"}, {duration: 1500, queue: false, easing: "swing"});
 
 });
